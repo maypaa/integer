338f96f042aa0bd1f3b2ed91ce7ac14671585346
maypaa <121723799+maypaa@users.noreply.github.com>
2023-01-17 17:04:16 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import { readFile, writeFile } from "node:fs/promises";
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.json());
+
+app.get("/api/baca", async (_req, res) => {
+  const text = await readFile("./data.txt", "utf8");
+  res.send(text);
+});
+
+app.post("/api/tulis", async (req, res) => {
+  await writeFile("./data.txt", req.body.text, "utf8");
+  res.send(`Berhasil menulis "${req.body.text}" ke file data.txt.`);
+});
+
+app.listen(3000, () => console.log("Server berjalan."));
